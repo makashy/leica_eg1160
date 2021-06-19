f6ee98ebfc1949f6a2b4536c66a519a5eb11b8dd
makashy <m.amin.kashi@gmail.com>
2021-06-19 12:53:34 +0430

    Create software project

---
+++ leica_eg1160 Software/leica_eg1160 Software/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * leica_eg1160 Software.cpp
+ *
+ * Created: 2021-06-19 12:47:55 PM
+ * Author : mamin
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
